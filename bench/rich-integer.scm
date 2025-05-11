f850479496c6c8dcfe31385ac3064b6678fce2cd
沈达 <da@liii.pro>
2025-05-11 07:40:17 +0000

    !428 Minor improvement on rich-integer bench

--- bench/rich-integer.scm
+++ bench/rich-integer.scm
@@ -12,48 +12,59 @@
         (repeat (- n 1) proc)))
 
 
-(timing "prim: " (lambda () (repeat 10000 (lambda () (number->string 65536)))))
+(timing "prim%to-string: " (lambda () (repeat 10000 (lambda () (number->string 65536)))))
 
 (define-case-class rich-integer2 ((data integer?))
   (define (%to-string)
     (number->string data)))
 
-(timing "rich-integer: " (lambda () (repeat 10000 (lambda () ((rich-integer 65536) :to-string)))))
-(timing "rich-integer2: " (lambda () (repeat 10000 (lambda () ((rich-integer2 65536) :to-string)))))
+(timing "rich-integer%to-string: " (lambda () (repeat 10000 (lambda () ((rich-integer 65536) :to-string)))))
+(timing "rich-integer2%to-string: " (lambda () (repeat 10000 (lambda () ((rich-integer2 65536) :to-string)))))
 
 (define (rint x)
-  (varlet (funclet rint) 'data x)
-
   (lambda (msg . args)
-    (case msg
-      ((:to-string)
-       (((funclet rint) '%to-string)))
-      ((:to-rich-string)
-       (((funclet rint) '%to-rich-string))))))
+    (let ((env (funclet rint)))
+      (case msg
+        ((:to-string)
+         (begin
+           (varlet env 'data x)
+           ((env '%to-string)) 
+           (cutlet env 'data)))
+        ((:to-rich-string)
+         (begin
+           (varlet env 'data x)
+           ((env '%to-rich-string))
+           (cutlet env 'data)))))))
 
-(varlet (funclet rint) '%to-string
-  (with-let (funclet rint)
-    (define (%to-string)
-      (number->string data))
-    %to-string))
+(with-let (funclet rint)
+  (define (%to-string)
+    (number->string data))
+  (varlet (funclet rint) '%to-string %to-string))
 
-(varlet (funclet rint) '%to-rich-string
-  (with-let (funclet rint)
-    (define (%to-rich-string)
-      (rich-string (%to-string)))
-    %to-rich-string))
+(with-let (funclet rint)
+  (define (%to-rich-string)
+    (rich-string (%to-string)))
+  (varlet (funclet rint) '%to-rich-string %to-rich-string))
 
-(varlet (funclet rint) '%sqrt
-  (with-let (funclet rint)
-    (define (%sqrt)
-      (if (< data 0)
-          (value-error
-            (format #f "sqrt of negative integer is undefined!         ** Got ~a **" data))
-          (inexact->exact (floor (sqrt data)))))))
+(with-let (funclet rint)
+  (define (%sqrt)
+        (if (< data 0)
+            (value-error
+              (format #f "sqrt of negative integer is undefined!         ** Got ~a **" data))
+            (inexact->exact (floor (sqrt data)))))
+  (varlet (funclet rint) '%sqrt %sqrt))
 
 
 
-(timing "rint: " (lambda () (repeat 10000 (lambda () ((rint 65536) :to-string)))))
+(timing "rint%to-string: " (lambda () (repeat 10000 (lambda () ((rint 65536) :to-string)))))
+
+(define (prim-sqrt data)
+  (if (< data 0)
+      (value-error
+        (format #f "sqrt of negative integer is undefined!         ** Got ~a **" data))
+      (inexact->exact (floor (sqrt data)))))
+
+(timing "prim%sqrt: " (lambda () (repeat 10000 (lambda () (prim-sqrt 65536)))))
 (timing "rint%sqrt: " (lambda () (repeat 10000 (lambda () ((rint 65536) :sqrt)))))
 (timing "rich-integer%sqrt: " (lambda () (repeat 10000 (lambda () ((rich-integer 65536) :sqrt)))))
 
