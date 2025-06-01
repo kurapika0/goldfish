9b63e37db26e006ecec3e3aa2bfc4a4eb042835c
沈达 <da@liii.pro>
2025-06-01 15:33:57 +0000

    !492 Bench on Chez Scheme, GNU Guile, Goldfish Scheme

--- bench/range.scala
+++ bench/range.scala
@@ -1,12 +1,11 @@
 @main def calculateSumOfSquares(): Unit =
   val startTime = System.nanoTime()
 
-  // 计算 1 到 10000 的平方和，重复 1000 次
   val results = (1 to 10000).map: _ =>
-    (1L to 10000L).count(x => x % 2 == 0)
+    (1L to 10000L).filter(x => x % 3 == 0)
 
   val endTime = System.nanoTime()
   val duration = (endTime - startTime) / 1e6d
 
-  println(s"Result (first iteration): ${results.head}")  // 打印第一个结果
+  println(s"Result (first iteration): ${results.head.length}")  // 打印第一个结果
   println(s"Total time for 1000 iterations: $duration msec")
--- bench/range.scm
+++ bench/range.scm
@@ -1,19 +1,31 @@
-(import (scheme time)
-        (liii range)
-        (liii list))
+(import (liii range)
+        (scheme time))
 
-(define start-time (current-jiffy))
+(((range 0 1000)
+  :map
+  (lambda (y)
+    (((range 0 10000) :map (lambda (x) (* x x)))
+     :filter (lambda (z) (zero? (modulo z 3)))
+     :collect)))
+ :collect)
 
-(define result
-  (((range 1 10000) :map
-    (lambda (y)
-      (((range 1 10000)
-        :map (lambda (x) (* x x)))
-       :count odd?)))
-   :collect))
-
-(display (car result))
+(display (rich-list :range 1 10 1 :collect))
 (newline)
 
+(define start-time (current-jiffy))
+(define result
+  ((rich-list :range 0 1000 1)
+   :map
+   (lambda (y)
+      ((rich-list :range 0 10000 1)
+       :map (lambda (x) (* x x))
+       :filter (lambda (z) (zero? (modulo z 3)))
+       :collect))
+   :collect))
 (define end-time (current-jiffy))
-(display (- end-time start-time))
+
+(display "Length of each result: ")
+(display (length (car result)))
+(newline)
+
+(display* "Elapsed time: " (/ (- end-time start-time) 1000.0) " ms\n")
